2658b307d80fda6822081467f5afd5d51f17fad8
leonkukuk <gigalitelk@gmail.com>
2018-08-16 20:28:05 +0200

    Add function to get a consistend UDID on both platforms

--- lib/flutter_udid.dart
+++ lib/flutter_udid.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
-
+import 'package:crypto/crypto.dart';
 import 'package:flutter/services.dart';
+import 'dart:convert';
 
 class FlutterUdid {
   static const MethodChannel _channel =
@@ -9,5 +10,12 @@ class FlutterUdid {
   static Future<String> get udid async {
     final String udid = await _channel.invokeMethod('getUDID');
     return udid;
+  }
+
+  static Future<String> get consistentUdid async {
+    final String udid = await _channel.invokeMethod('getUDID');
+    var bytes = utf8.encode(udid);
+    var digest = sha256.convert(bytes);
+    return digest.toString();
   }
 }
