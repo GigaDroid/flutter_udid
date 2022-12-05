d7ecefed0aebb84de43e1ebad854fdc912e242a1
Mikhail Rymarev <qqmikey@icloud.com>
2022-12-04 23:07:25 -0500

    Update SwiftFlutterUdidPlugin.swift

--- ios/Classes/SwiftFlutterUdidPlugin.swift
+++ ios/Classes/SwiftFlutterUdidPlugin.swift
@@ -21,23 +21,7 @@ public class SwiftFlutterUdidPlugin: NSObject, FlutterPlugin {
         let bundleName = Bundle.main.infoDictionary!["CFBundleName"] as! String
         let accountName = Bundle.main.bundleIdentifier!
         
-        var applicationUUID = SAMKeychain.password(forService: bundleName, account: accountName)
-        
-        if applicationUUID == nil {
-            
-            applicationUUID = (UIDevice.current.identifierForVendor?.uuidString)!
-            let query = SAMKeychainQuery()
-            query.service = bundleName
-            query.account = accountName
-            query.password = applicationUUID
-            query.synchronizationMode = SAMKeychainQuerySynchronizationMode.no
-            
-            do {
-                try query.save()
-            } catch let error as NSError {
-                print("SAMKeychainQuery Exception: \(error)")
-            }
-        }
+        var applicationUUID = (UIDevice.current.identifierForVendor?.uuidString)!
         
         if(applicationUUID==nil||applicationUUID==""){
             result(FlutterError.init(code: "UNAVAILABLE",
